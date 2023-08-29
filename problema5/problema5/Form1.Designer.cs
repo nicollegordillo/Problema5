6fe3aa256b2b9079633681daf5c863c5583f14dc
nicollegordillo <98134296+nicollegordillo@users.noreply.github.com>
2023-08-28 23:02:47 -0600

    Programa

---
+++ problema5/problema5/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace problema5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1=new PictureBox();
+            radioButton1=new RadioButton();
+            radioButton2=new RadioButton();
+            radioButton3=new RadioButton();
+            textBox1=new TextBox();
+            label1=new Label();
+            label2=new Label();
+            textBox2=new TextBox();
+            textBox3=new TextBox();
+            label3=new Label();
+            textBox4=new TextBox();
+            label4=new Label();
+            label5=new Label();
+            textBox5=new TextBox();
+            label6=new Label();
+            button1=new Button();
+            textBox6=new TextBox();
+            label7=new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location=new Point(12, 12);
+            pictureBox1.Name="pictureBox1";
+            pictureBox1.Size=new Size(649, 439);
+            pictureBox1.TabIndex=0;
+            pictureBox1.TabStop=false;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize=true;
+            radioButton1.Location=new Point(836, 27);
+            radioButton1.Name="radioButton1";
+            radioButton1.Size=new Size(117, 24);
+            radioButton1.TabIndex=1;
+            radioButton1.TabStop=true;
+            radioButton1.Text="Cono circular";
+            radioButton1.UseVisualStyleBackColor=true;
+            radioButton1.CheckedChanged+=radioButton1_CheckedChanged;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize=true;
+            radioButton2.Location=new Point(836, 68);
+            radioButton2.Name="radioButton2";
+            radioButton2.Size=new Size(133, 24);
+            radioButton2.TabIndex=2;
+            radioButton2.TabStop=true;
+            radioButton2.Text="Tronco de cono";
+            radioButton2.UseVisualStyleBackColor=true;
+            radioButton2.CheckedChanged+=radioButton2_CheckedChanged;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize=true;
+            radioButton3.Location=new Point(836, 112);
+            radioButton3.Name="radioButton3";
+            radioButton3.Size=new Size(103, 24);
+            radioButton3.TabIndex=3;
+            radioButton3.TabStop=true;
+            radioButton3.Text="Hemisferio";
+            radioButton3.UseVisualStyleBackColor=true;
+            radioButton3.CheckedChanged+=radioButton3_CheckedChanged;
+            // 
+            // textBox1
+            // 
+            textBox1.Location=new Point(845, 271);
+            textBox1.Name="textBox1";
+            textBox1.Size=new Size(125, 27);
+            textBox1.TabIndex=4;
+            textBox1.Visible=false;
+            // 
+            // label1
+            // 
+            label1.AutoSize=true;
+            label1.Location=new Point(768, 274);
+            label1.Name="label1";
+            label1.Size=new Size(71, 20);
+            label1.TabIndex=5;
+            label1.Text="Radio(m)";
+            label1.Visible=false;
+            // 
+            // label2
+            // 
+            label2.AutoSize=true;
+            label2.Location=new Point(767, 320);
+            label2.Name="label2";
+            label2.Size=new Size(72, 20);
+            label2.TabIndex=6;
+            label2.Text="Altura(m)";
+            label2.Visible=false;
+            // 
+            // textBox2
+            // 
+            textBox2.Location=new Point(845, 317);
+            textBox2.Name="textBox2";
+            textBox2.Size=new Size(125, 27);
+            textBox2.TabIndex=7;
+            textBox2.Visible=false;
+            // 
+            // textBox3
+            // 
+            textBox3.Location=new Point(845, 365);
+            textBox3.Name="textBox3";
+            textBox3.Size=new Size(125, 27);
+            textBox3.TabIndex=9;
+            textBox3.Visible=false;
+            // 
+            // label3
+            // 
+            label3.AutoSize=true;
+            label3.Location=new Point(717, 368);
+            label3.Name="label3";
+            label3.Size=new Size(122, 20);
+            label3.TabIndex=8;
+            label3.Text="Radio menor (m)";
+            label3.Visible=false;
+            // 
+            // textBox4
+            // 
+            textBox4.Location=new Point(845, 220);
+            textBox4.Name="textBox4";
+            textBox4.Size=new Size(125, 27);
+            textBox4.TabIndex=13;
+            textBox4.Visible=false;
+            // 
+            // label4
+            // 
+            label4.AutoSize=true;
+            label4.Location=new Point(817, 220);
+            label4.Name="label4";
+            label4.Size=new Size(22, 20);
+            label4.TabIndex=12;
+            label4.Text="P(";
+            label4.Visible=false;
+            // 
+            // label5
+            // 
+            label5.AutoSize=true;
+            label5.Location=new Point(807, 177);
+            label5.Name="label5";
+            label5.Size=new Size(39, 20);
+            label5.TabIndex=11;
+            label5.Text="Q(C)";
+            label5.Visible=false;
+            // 
+            // textBox5
+            // 
+            textBox5.Location=new Point(845, 174);
+            textBox5.Name="textBox5";
+            textBox5.Size=new Size(125, 27);
+            textBox5.TabIndex=10;
+            textBox5.Visible=false;
+            // 
+            // label6
+            // 
+            label6.AutoSize=true;
+            label6.Location=new Point(976, 223);
+            label6.Name="label6";
+            label6.Size=new Size(46, 20);
+            label6.TabIndex=14;
+            label6.Text=", 0) m";
+            label6.Visible=false;
+            // 
+            // button1
+            // 
+            button1.Location=new Point(859, 422);
+            button1.Name="button1";
+            button1.Size=new Size(94, 29);
+            button1.TabIndex=15;
+            button1.Text="Calcular";
+            button1.UseVisualStyleBackColor=true;
+            button1.Click+=button1_Click;
+            // 
+            // textBox6
+            // 
+            textBox6.Location=new Point(845, 475);
+            textBox6.Name="textBox6";
+            textBox6.Size=new Size(125, 27);
+            textBox6.TabIndex=17;
+            textBox6.Visible=false;
+            // 
+            // label7
+            // 
+            label7.AutoSize=true;
+            label7.Location=new Point(793, 478);
+            label7.Name="label7";
+            label7.Size=new Size(53, 20);
+            label7.TabIndex=16;
+            label7.Text="E(N/C)";
+            label7.Visible=false;
+            label7.Click+=label7_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions=new SizeF(8F, 20F);
+            AutoScaleMode=AutoScaleMode.Font;
+            ClientSize=new Size(1098, 676);
+            Controls.Add(textBox6);
+            Controls.Add(label7);
+            Controls.Add(button1);
+            Controls.Add(label6);
+            Controls.Add(textBox4);
+            Controls.Add(label4);
+            Controls.Add(label5);
+            Controls.Add(textBox5);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(radioButton3);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(pictureBox1);
+            Name="Form1";
+            Text="Form1";
+            Load+=Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label3;
+        private TextBox textBox4;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox5;
+        private Label label6;
+        private Button button1;
+        private TextBox textBox6;
+        private Label label7;
+    }
+}
---
+++ problema5/problema5/Form1.cs
@@ -0,0 +1,289 @@
+using Microsoft.VisualBasic.Devices;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+//Programa de Paula Barillas y Nicolle Gordillo
+namespace problema5
+{
+    public partial class Form1 : Form
+    {
+
+        //private Point[] dotPositions = new Point[0];
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Paint += pictureBoxCampo_Paint;
+            pictureBox1.Width = 601; // Adjust this value as needed
+            pictureBox1.Height = 601; // Adjust this value as needed
+                                      // dotPositions = new Point[0];
+        }
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Invalidate(); // Redraw the PictureBox
+            label7.Visible=false;
+            textBox6.Visible=false;
+            label4.Visible=true;
+            label5.Visible=true;
+            label6.Visible=true;
+            textBox4.Visible=true;
+            textBox5.Visible=true;
+            label1.Visible=true;
+            label2.Visible=true;
+            label3.Visible=false;
+            textBox1.Visible=true;
+            textBox2.Visible=true; ;
+            textBox3.Visible=false;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Invalidate(); // Redraw the PictureBox
+            label7.Visible=false;
+            textBox6.Visible=false;
+            label4.Visible=true;
+            label5.Visible=true;
+            label6.Visible=true;
+            textBox4.Visible=true;
+            textBox5.Visible=true;
+            label1.Visible=true;
+            label2.Visible=true;
+            label3.Visible=true;
+            textBox1.Visible=true;
+            textBox2.Visible=true;
+            textBox3.Visible=true;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Invalidate(); // Redraw the PictureBox
+            label7.Visible=false;
+            textBox6.Visible=false;
+            label4.Visible=true;
+            label5.Visible=true;
+            label6.Visible=true;
+            textBox4.Visible=true;
+            textBox5.Visible=true;
+            label1.Visible=true;
+            label2.Visible=false;
+            label3.Visible=false;
+            textBox1.Visible=true;
+            textBox2.Visible=false;
+            textBox3.Visible=false;
+        }
+
+        private void pictureBoxCampo_Paint(object sender, PaintEventArgs e)
+        {
+            // Dibuja la cuadrícula en el PictureBox
+            DrawGrid(e.Graphics);
+
+            // Determine which radio button is checked and call the corresponding drawing method
+            /*
+            if (radioButton1.Checked)
+            {
+                DrawCone(e.Graphics, pictureBox1.Width, pictureBox1.Height);
+            }
+            else if (radioButton2.Checked)
+            {
+                DrawTruncatedCone(e.Graphics, pictureBox1.Width, pictureBox1.Height);
+            }
+            else if (radioButton3.Checked)
+            {
+                DrawHemisphere(e.Graphics, pictureBox1.Width, pictureBox1.Height);
+            }*/
+        }
+
+
+        private void DrawGrid(Graphics g)
+        {
+            int gridSize = 20; // Tamaño de cada celda de la cuadrícula
+
+            // Obtiene el tamaño del PictureBox
+            int pictureBoxWidth = 600;
+            int pictureBoxHeight = 600;
+
+            // Dibuja las líneas verticales de la cuadrícula
+            for (int x = 0; x <= pictureBoxWidth; x += gridSize)
+            {
+                g.DrawLine(Pens.DarkGray, x, 0, x, pictureBoxHeight);
+            }
+
+            // Dibuja las líneas horizontales de la cuadrícula
+            for (int y = 0; y <= pictureBoxHeight; y += gridSize)
+            {
+                g.DrawLine(Pens.DarkGray, 0, y, pictureBoxWidth, y);
+            }
+
+            // Dibuja el eje X (horizontal) en el centro del PictureBox
+            g.DrawLine(Pens.Black, 0, pictureBoxHeight / 2, pictureBoxWidth, pictureBoxHeight / 2);
+
+            // Dibuja el eje Y (vertical) en el centro del PictureBox
+            g.DrawLine(Pens.Black, pictureBoxWidth / 2, 0, pictureBoxWidth / 2, pictureBoxHeight);
+            /*
+            // Determine which radio button is checked
+            if (radioButton1.Checked)
+            {
+                DrawCone(g, pictureBoxWidth, pictureBoxHeight);
+            }
+            else if (radioButton2.Checked)
+            {
+                DrawTruncatedCone(g, pictureBoxWidth, pictureBoxHeight);
+            }
+            else if (radioButton3.Checked)
+            {
+                DrawHemisphere(g, pictureBoxWidth, pictureBoxHeight);
+            }*/
+        }
+
+        private void DrawCone(Graphics g, int pictureBoxWidth, int pictureBoxHeight)
+        {
+            int centerX = pictureBoxWidth / 2;
+            int centerY = pictureBoxHeight / 2;
+            double altura = Convert.ToDouble(textBox2.Text)*20;
+            double radio = Convert.ToDouble(textBox1.Text)*20;
+            // Draw the cone using lines
+            Point[] coneVertices = new Point[]
+            {
+
+                new Point(centerX , centerY -(int)radio),
+                new Point(centerX+(int)altura, centerY),
+                new Point(centerX, centerY +(int)radio)
+            };
+            g.DrawPolygon(Pens.Blue, coneVertices);
+        }
+
+        private void DrawTruncatedCone(Graphics g, int pictureBoxWidth, int pictureBoxHeight)
+        {
+            int centerX = pictureBoxWidth / 2;
+            int centerY = pictureBoxHeight / 2;
+            double altura = Convert.ToDouble(textBox2.Text)*20;
+            double radio = Convert.ToDouble(textBox1.Text)*20;
+            double radiom = Convert.ToDouble(textBox3.Text)*20;
+            // Draw the truncated cone using lines
+            Point[] truncatedConeVertices = new Point[]
+            {
+                new Point(centerX , centerY -(int)radio),
+                new Point(centerX+(int)altura, centerY-(int)radiom),
+                new Point(centerX+(int)altura, centerY+(int)radiom),
+                new Point(centerX, centerY +(int)radio)
+            };
+            g.DrawPolygon(Pens.Red, truncatedConeVertices);
+        }
+
+        private void DrawHemisphere(Graphics g, int pictureBoxWidth, int pictureBoxHeight)
+        {
+            int centerX = pictureBoxWidth / 2;
+            int centerY = pictureBoxHeight / 2;
+            double radius = Convert.ToDouble(textBox1.Text)*20;
+
+            // Draw the hemisphere using an ellipse and a chord
+            g.DrawPie(Pens.Purple, centerX -(int)radius, centerY - (int)radius, 2 *(int)radius, (int)radius*2, 90, 180);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label7.Visible=true;
+            textBox6.Visible=true;
+            // Get the center point of the X-axis
+            int centerX = pictureBox1.Width / 2;
+            int centerY = pictureBox1.Height / 2;
+
+            // Draw a small ellipse (dot) at the center of the X-axis
+            int dotRadius = 5; // Adjust this value as needed
+            int dotX = centerX - dotRadius;
+            int dotY = centerY - dotRadius;
+            int dotDiameter = 2 * dotRadius;
+            /*
+            foreach (Point dotPosition in dotPositions)
+            {
+                pictureBox1.CreateGraphics().FillEllipse(Brushes.HotPink, dotPosition.X - dotRadius, dotPosition.Y - dotRadius, 2 * dotRadius, 2 * dotRadius);
+            }*/
+
+            double x = Convert.ToDouble(textBox4.Text)*20;
+            if (radioButton1.Checked)
+            {
+                DrawCone(pictureBox1.CreateGraphics(), 601, 601);
+
+                double Q = Convert.ToDouble(textBox5.Text);
+                double R = Convert.ToDouble(textBox1.Text);
+                double H = Convert.ToDouble(textBox2.Text);
+                double d = Convert.ToDouble(textBox4.Text);
+                double electricField = Math.Round((((3*Q)/(2*Math.PI*(8.85*Math.Pow(10, -1*12))*Math.Pow(R, 3)))*(H-Math.Log(R+(Math.Sqrt(Math.Pow(d+H, 2)+Math.Pow(R, 2))/(d+H))))), 2);
+                // Calculate the end point of the arrow
+                int arrowEndX = (int)d;
+                textBox6.Text=electricField.ToString();
+                // Draw the arrow with magnitude-based length
+                DrawArrow(pictureBox1.CreateGraphics(), centerX, centerY, arrowEndX, electricField);
+            }
+            else if (radioButton2.Checked)
+            {
+                DrawTruncatedCone(pictureBox1.CreateGraphics(), 601, 601);
+                double Q = Convert.ToDouble(textBox5.Text);
+                double r = Convert.ToDouble(textBox1.Text);
+                double H = Convert.ToDouble(textBox2.Text);
+                double d = Convert.ToDouble(textBox4.Text);
+                double R = Convert.ToDouble(textBox3.Text);
+
+                //double electricField = Math.Round((-1*Math.Pow(H,2)*Math.Sqrt((d*d*(r-R)*(r-R)-2*d*(r-R)*(H*R+d*(r-R))+H*H*(R*R-d*d)+2*H*R*d*(r-R)+d*d*(r-R)*(r-R))/(H*H))/(H*H+(r-R)*(r-R)))+((H*(r-R)*(d*(R-r)+H*R))/(Math.Sqrt(Math.Pow((H*H+(r-R)*(r-R)),3))))*(Math.Atanh(((-1*d*(r-R)*(r-R)+H*H*d+H*R*(r-R+d*(r-R*R)))/(H*Math.Sqrt(H*H+(r-R)*(r-R)*((1/(H*H))*(d*d*(r-R)*(r-R))-2*d*(r-R)*(H*R+d*(r-R))+H*H*(R*R+d*d)+2*H*R*d*(r-R)+d*d*(r-R)*(r-R)))))))-1*(-1*Math.Pow(H, 2)*Math.Sqrt((d*d*(r-R)*(r-R)-2*d*(r-R)*(H*R+(d+H)*(r-R))+H*H*(R*R-(d+H)*(d*H))+2*H*R*(d+H)*(r-R)+(d+H)*(d+H)*(r-R)*(r-R))/(H*H))/(H*H+(r-R)*(r-R)))+((H*(r-R)*(d*(R-r)+H*R))/(Math.Sqrt(Math.Pow((H*H+(r-R)*(r-R)), 3))))*(Math.Atanh(((-1*d*(r-R)*(r-R)+H*H*(d+H)+H*R*(r-R+(d+H)*(r-R*R)))/(H*Math.Sqrt(H*H+(r-R)*(r-R)*((1/(H*H))*(d*d*(r-R)*(r-R))-2*d*(r-R)*(H*R+(d+H)*(r-R))+H*H*(R*R+(d+H)*(d+H))+2*H*R*(d+H)*(r-R)+(d+H)*(d+H)*(r-R)*(r-R))))))), 2);
+                // Calculate the end point of the arrow
+                double electricField = Math.Round((((3*Q)/(2*Math.PI*(8.85*Math.Pow(10, -1*12))*Math.Pow(R, 3)))*(H-Math.Log(R+(Math.Sqrt(Math.Pow(d+H, 2)+Math.Pow(R, 2))/(d+H))))), 2);
+                int arrowEndX = (int)d;
+                textBox6.Text=electricField.ToString();
+                // Draw the arrow with magnitude-based length
+                DrawArrow(pictureBox1.CreateGraphics(), centerX, centerY, arrowEndX, electricField);
+            }
+            else if (radioButton3.Checked)
+            {
+                DrawHemisphere(pictureBox1.CreateGraphics(), 601, 601);
+                double Q = Convert.ToDouble(textBox5.Text);
+                double R = Convert.ToDouble(textBox1.Text);
+                double d = Convert.ToDouble(textBox4.Text);
+                double electricField = Math.Round((Q/(6*Math.PI*Math.Pow(10, -1*12)*Math.Pow(R, 2)))*(R-(Math.Sqrt(Math.Pow(d, 2)+Math.Pow(R, 2))*(-1*2*Math.Pow(d, 2)+Math.Pow(R, 2))-(d-R)*(-2*d*d+d*R+R*R))/(3*d*d)), 2);
+                // Calculate the end point of the arrow
+                int arrowEndX = (int)d;
+                textBox6.Text=electricField.ToString();
+                // Draw the arrow with magnitude-based length
+                DrawArrow(pictureBox1.CreateGraphics(), centerX, centerY, arrowEndX, electricField);
+            }
+            pictureBox1.CreateGraphics().FillPie(Brushes.HotPink, centerX- dotRadius+(int)x, centerY- dotRadius, 2 * dotRadius, dotRadius*2, 0, 360);
+            /*
+            Array.Resize(ref dotPositions, dotPositions.Length + 1);
+            dotPositions[dotPositions.Length - 1] = new Point(centerX  + (int)x, centerY);*/
+        }
+        private void DrawArrow(Graphics g, int startX, int startY, int endX, double magnitude)
+        {
+            Pen arrowPen = new Pen(Color.CornflowerBlue, 2);
+            double tamaño = magnitude/500000000;
+            int dir;
+            if (endX<0)
+            {
+                dir = -1;
+            }
+            else
+            {
+                dir = 1;
+            }
+            // Draw the arrow body as a line
+            g.DrawLine(arrowPen, startX, startY, startX+20*(int)tamaño*dir, startY);
+
+            // Draw the arrowhead
+            Point[] coneVertices = new Point[]
+            {
+                new Point(startX+20*(int)tamaño*dir, startY+5),
+                new Point(startX+25*(int)tamaño*dir, startY),
+                new Point(startX+20*(int)tamaño*dir, startY-5)
+            };
+            g.DrawPolygon(arrowPen, coneVertices);
+        }
+        private void label7_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
